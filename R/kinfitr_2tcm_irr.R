9b1e36cb34d69459588c5966b9bd87d1c0bedca6
mathesong <mathesong@gmail.com>
2024-08-14 10:44:02 +0200

    Added an irreversible 2TC model to fix the AIC calculations

--- R/kinfitr_2tcm_irr.R
+++ R/kinfitr_2tcm_irr.R
@@ -1,6 +1,6 @@
-#' Two Tissue Compartment Model
+#' Two Tissue Compartment Model with Irreversible Specific Binding
 #'
-#' Function to fit the Two Tissue Compartment Model to data.
+#' Function to fit the Two Tissue Compartment Model with irreversible specific binding to data.
 #'
 #' @param t_tac Numeric vector of times for each frame in minutes. We use the
 #'   time halfway through the frame as well as a zero. If a time zero frame is
@@ -41,11 +41,6 @@
 #' @param k3.lower Optional. Lower bound for the fitting of k3. Default is
 #'   0.0001.
 #' @param k3.upper Optional. Upper bound for the fitting of k3. Default is 0.5.
-#' @param k4.start Optional. Starting parameter for fitting of k4. Default is
-#'   0.1.
-#' @param k4.lower Optional. Lower bound for the fitting of k4. Default is
-#'   0.0001.
-#' @param k4.upper Optional. Upper bound for the fitting of k4. Default is 0.5.
 #' @param inpshift.start Optional. Starting parameter for fitting of inpshift.
 #'   Default is 0.
 #' @param inpshift.lower Optional. Lower bound for the fitting of inpshift.
@@ -92,18 +87,17 @@
 #'   t_parentfrac = 1, parentfrac = 1
 #' )
 #'
-#' fit1 <- twotcm(t_tac, tac, input, weights)
-#' fit2 <- twotcm(t_tac, tac, input, weights, inpshift = 0.1, vB = 0.05)
+#' fit1 <- twotcm_irr(t_tac, tac, input, weights)
+#' fit2 <- twotcm_irr(t_tac, tac, input, weights, inpshift = 0.1, vB = 0.05)
 #' @author Granville J Matheson, \email{mathesong@@gmail.com}
 #'
 #' @export
 
-twotcm <- function(t_tac, tac, input, weights = NULL, inpshift = NULL, vB = NULL,
+twotcm_irr <- function(t_tac, tac, input, weights = NULL, inpshift = NULL, vB = NULL,
                    frameStartEnd = NULL,
                    K1.start = 0.1, K1.lower = 0.0001, K1.upper = 1,
                    k2.start = 0.1, k2.lower = 0.0001, k2.upper = 0.5,
                    k3.start = 0.1, k3.lower = 0.0001, k3.upper = 0.5,
-                   k4.start = 0.1, k4.lower = 0.0001, k4.upper = 0.5,
                    inpshift.start = 0, inpshift.lower = -0.5, inpshift.upper = 0.5,
                    vB.start = 0.05, vB.lower = 0.01, vB.upper = 0.1,
                    multstart_iter = 1, multstart_lower = NULL, multstart_upper = NULL,
@@ -123,15 +117,15 @@ twotcm <- function(t_tac, tac, input, weights = NULL, inpshift = NULL, vB = NULL
   # Parameters
 
   start <- c(
-    K1 = K1.start, k2 = k2.start, k3 = k3.start, k4 = k4.start,
+    K1 = K1.start, k2 = k2.start, k3 = k3.start,
     inpshift = inpshift.start, vB = vB.start
   )
   lower <- c(
-    K1 = K1.lower, k2 = k2.lower, k3 = k3.lower, k4 = k4.lower,
+    K1 = K1.lower, k2 = k2.lower, k3 = k3.lower,
     inpshift = inpshift.lower, vB = vB.lower
   )
   upper <- c(
-    K1 = K1.upper, k2 = k2.upper, k3 = k3.upper, k4 = k4.upper,
+    K1 = K1.upper, k2 = k2.upper, k3 = k3.upper,
     inpshift = inpshift.upper, vB = vB.upper
   )
 
@@ -180,7 +174,7 @@ twotcm <- function(t_tac, tac, input, weights = NULL, inpshift = NULL, vB = NULL
     modeldata$input <- newvals$input
 
     formula <- paste(
-      "tac ~ twotcm_model(t_tac, input, K1, k2, k3, k4, vB",
+      "tac ~ twotcm_model(t_tac, input, K1, k2, k3, k4=0, vB",
       ifelse( vB_fitted, yes = "", no = paste0("=", vB)),
       ")", sep=""
     )
@@ -212,7 +206,7 @@ twotcm <- function(t_tac, tac, input, weights = NULL, inpshift = NULL, vB = NULL
     inpshift_fitted <- T
 
     formula <- paste(
-      "tac ~ twotcm_fitDelay_model(t_tac, input, K1, k2, k3, k4, inpshift, vB",
+      "tac ~ twotcm_fitDelay_model(t_tac, input, K1, k2, k3, k4=0, inpshift, vB",
       ifelse( vB_fitted, yes = "", no = paste0("=", vB)),
       ")", sep=""
     )
@@ -280,17 +274,14 @@ twotcm <- function(t_tac, tac, input, weights = NULL, inpshift = NULL, vB = NULL
   par.se[1,] <- purrr::map_dbl(names(par), ~ get_se(output, .x))
   names(par.se) <- paste0(names(par.se), ".se")
 
-  par$Vt <- (par$K1 / par$k2) * (1 + par$k3 / par$k4)
-  par.se$Vt.se <- get_se(output, "(K1/k2) * (1+(k3/k4))")
+  par$Ki <- (par$K1 * par$k3) / (par$k2 + par$k3)
+  par.se$Ki.se <- get_se(output, "(K1 * k3) / (k2 + k3)")
 
   par$Vnd <- (par$K1 / par$k2)
   par.se$Vnd.se <- get_se(output, "(K1/k2)")
 
-  par$BPp <- (par$K1 / par$k2) * (par$k3 / par$k4)
-  par.se$BPp.se <- get_se(output, "(K1/k2)*(k3/k4)")
-
-  par$BPnd <- (par$k3 / par$k4)
-  par.se$BPnd.se <- get_se(output, "(k3/k4)")
+  par$lambdak3 <- (par$K1 / par$k2) * par$k3
+  par.se$lambdak3.se <- get_se(output, "(K1/k2)*k3")
 
 
 
@@ -303,219 +294,4 @@ twotcm <- function(t_tac, tac, input, weights = NULL, inpshift = NULL, vB = NULL
   class(out) <- c("2tcm", "kinfit")
 
   return(out)
-}
-
-
-#' Model: Two Tissue Compartment Model
-#'
-#' This is the Two Tissue Compartment Model model itself by which predicted values are generated.
-#'
-#' @param t_tac Numeric vector of times for each frame in minutes. We use the time halfway through the frame as well as a zero.
-#' @param input Data frame containing the blood, plasma, and parent fraction concentrations over time.  This can be generated
-#' using the \code{blood_interp} function.
-#' @param K1 Parameter value for K1
-#' @param k2 Parameter value for k2
-#' @param k3 Parameter value for k3
-#' @param k4 Parameter value for k4
-#' @param vB Parameter value for vB
-#'
-#' @return A numeric vector of the predicted values of the TAC in the target region.
-#'
-#' @examples
-#' \dontrun{
-#' twotcm_model(t_tac, input, K1 = 0.1, k2 = 0.08, k3 = 0.05, k4 = 0.02, vB = 0.05)
-#' }
-#'
-#' @author Granville J Matheson, \email{mathesong@@gmail.com}
-#'
-#' @export
-
-twotcm_model <- function(t_tac, input, K1, k2, k3, k4, vB) {
-  interptime <- input$Time
-  step <- interptime[2] - interptime[1]
-
-  i_blood <- input$Blood
-  aif <- input$AIF
-
-  delta <- sqrt((k2 + k3 + k4)^2 - 4 * k2 * k4)
-  th1 <- (k2 + k3 + k4 + delta) / 2
-  th2 <- (k2 + k3 + k4 - delta) / 2
-  ph1 <- K1 * (th1 - k3 - k4) / delta
-  ph2 <- K1 * (th2 - k3 - k4) / (-delta)
-
-  a <- ph1 * exp(-th1 * interptime) + ph2 * exp(-th2 * interptime)
-  b <- aif
-
-  i_outtac <- kinfit_convolve(a, b, step)
-
-  # Correction for vB
-  i_outtac <- i_outtac * (1 - vB) + vB * i_blood
-
-  outtac <- pracma::interp1(interptime, i_outtac, t_tac)
-
-  return(outtac)
-}
-
-
-#' Model: Two Tissue Compartment Model with Delay
-#'
-#' This is the Two Tissue Compartment Model model itself by which predicted values are generated, which includes fitting of the
-#' delay, inpshift.
-#'
-#' @param t_tac Numeric vector of times for each frame in minutes. We use the time halfway through the frame as well as a zero.
-#' @param input Data frame containing the blood, plasma, and parent fraction concentrations over time.  This can be generated
-#' using the \code{blood_interp} function.
-#' @param K1 Parameter value for K1
-#' @param k2 Parameter value for k2
-#' @param k3 Parameter value for k3
-#' @param k4 Parameter value for k4
-#' @param inpshift Parameter value for inpshift, the delay.
-#' @param vB Parameter value for vB
-#'
-#' @return A numeric vector of the predicted values of the TAC in the target region.
-#'
-#' @examples
-#' \dontrun{
-#' twotcm_model(t_tac, input, K1 = 0.1, k2 = 0.08, k3 = 0.05, k4 = 0.02, vB = 0.05)
-#' }
-#'
-#' @author Granville J Matheson, \email{mathesong@@gmail.com}
-#'
-#' @export
-
-twotcm_fitDelay_model <- function(t_tac, input, K1, k2, k3, k4, inpshift, vB) {
-  newvals <- shift_timings(t_tac, rep(1, length(t_tac)), input, inpshift) # Using ones instead of tac as don't need it
-
-  t_tac <- newvals$t_tac
-
-  i_blood <- newvals$input$Blood
-  aif <- newvals$input$AIF
-
-  interptime <- newvals$input$Time
-  step <- interptime[2] - interptime[1]
-
-  delta <- sqrt((k2 + k3 + k4)^2 - 4 * k2 * k4)
-  th1 <- (k2 + k3 + k4 + delta) / 2
-  th2 <- (k2 + k3 + k4 - delta) / 2
-  ph1 <- K1 * (th1 - k3 - k4) / delta
-  ph2 <- K1 * (th2 - k3 - k4) / (-delta)
-
-  a <- ph1 * exp(-th1 * interptime) + ph2 * exp(-th2 * interptime)
-  b <- aif
-
-  i_outtac <- kinfit_convolve(a, b, step)
-
-  # Correction for vB
-  i_outtac <- i_outtac * (1 - vB) + vB * aif
-
-  outtac <- pracma::interp1(interptime, i_outtac, t_tac)
-
-  return(outtac)
-}
-
-
-#' Plot: Two Tissue Compartment Model
-#'
-#' Function to visualise the fit of the Two Tissue Compartment Model to data.
-#'
-#' @param twotcmout The output object of the 2TCM fitting procedure.
-#' @param roiname Optional. The name of the Target Region to see it on the plot.
-#'
-#' @return A ggplot2 object of the plot.
-#'
-#' @examples
-#'
-#' data(pbr28)
-#'
-#' t_tac <- pbr28$tacs[[2]]$Times / 60
-#' tac <- pbr28$tacs[[2]]$FC
-#' weights <- pbr28$tacs[[2]]$Weights
-#'
-#' input <- blood_interp(
-#'   pbr28$procblood[[2]]$Time / 60, pbr28$procblood[[2]]$Cbl_dispcorr,
-#'   pbr28$procblood[[2]]$Time / 60, pbr28$procblood[[2]]$Cpl_metabcorr,
-#'   t_parentfrac = 1, parentfrac = 1
-#' )
-#'
-#' fit <- twotcm(t_tac, tac, input, weights, inpshift = 0.1, vB = 0.05)
-#'
-#' plot_2tcmfit(fit)
-#' @author Granville J Matheson, \email{mathesong@@gmail.com}
-#'
-#' @import ggplot2
-#'
-#' @export
-
-plot_2tcmfit <- function(twotcmout, roiname = NULL) {
-  if (is.null(roiname)) {
-    roiname <- "ROI"
-  }
-
-  measureddf <- data.frame(
-    Time = twotcmout$tacs$Time,
-    Radioactivity = twotcmout$tacs$Target,
-    Weights = weights(twotcmout$fit),
-    Region = paste0(roiname, ".Measured")
-  )
-
-  inputdf <- data.frame(
-    Time = twotcmout$input$Time,
-    Radioactivity = twotcmout$input$AIF,
-    Weights = 1,
-    Region = "AIF"
-  )
-
-  i_fit <- predict(twotcmout$fit, newdata = list(
-    t_tac = twotcmout$input$Time,
-    tac = pracma::interp1(
-      twotcmout$tacs$Time,
-      twotcmout$tacs$Target,
-      twotcmout$input$Time,
-      method = "linear"
-    )
-  ))
-
-  # Correct for TAC shifting
-  if( twotcmout$inpshift_fitted ) {
-    mod_tac_start <- head(twotcmout$fit$m$getEnv()$t_tac)[-1]
-    dat_tac_start <- head(twotcmout$tacs$Time)[-1]
-    diffs <- dat_tac_start - mod_tac_start
-
-    equal_diffs <- abs(max(diffs) - min(diffs)) < 10e-3
-
-    if(equal_diffs) {
-      tacshift <- mean(diffs)
-    } else {
-      tacshift <- 0
-      warning("Cannot infer whether TAC was shifted. Fit line might be misaligned in time.")
-    }
-  } else {
-    tacshift <- 0
-  }
-
-  fitdf <- data.frame(
-    Time = twotcmout$input$Time + tacshift,
-    Radioactivity = i_fit,
-    Weights = 1, Region = paste0(roiname, ".Fitted")
-  )
-
-  plotdf <- rbind(inputdf, measureddf, fitdf)
-
-  plotdf$Region <- forcats::fct_inorder(factor(plotdf$Region))
-
-  myColors <- RColorBrewer::brewer.pal(3, "Set1")
-  names(myColors) <- levels(plotdf$Region)
-  colScale <- scale_colour_manual(name = "Region", values = myColors)
-
-  #   if(roiname != 'ROI') {
-  #     plotdf$Region[plotdf$Region=='Measured'] = paste0(roiname, '')
-  #   }
-
-  outplot <- ggplot(plotdf, aes(x = Time, y = Radioactivity, colour = Region)) + colScale +
-    geom_point(data = subset(plotdf, plotdf$Region == paste0(roiname, ".Measured")), aes(shape = "a", size = Weights)) +
-    geom_line(data = subset(plotdf, plotdf$Region != paste0(roiname, ".Measured"))) +
-    guides(shape = "none", color = guide_legend(order = 1)) + scale_size(range = c(1, 3)) + coord_cartesian(ylim = c(0, max(measureddf$Radioactivity) * 1.5))
-
-  # print(outplot)
-  return(outplot)
 }
