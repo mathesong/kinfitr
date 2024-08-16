0cae832bf0b90c93edc6333aa8a36e33c012820e
mathesong <mathesong@gmail.com>
2024-08-17 00:56:55 +0200

    Added an alternative macroparameter parameterisation of the 2TCM

--- R/kinfitr_2tcm_macro.R
+++ R/kinfitr_2tcm_macro.R
@@ -1,6 +1,9 @@
-#' Two Tissue Compartment Model
+#' Two Tissue Compartment Model with Alternative Macroparameter Parameterisation
 #'
-#' Function to fit the Two Tissue Compartment Model to data.
+#' Function to fit the Two Tissue Compartment Model with an alternative
+#' parameterisation to data. This gives some more fine-grained control over
+#' setting parameter limits and setting parameter values from previously
+#' estimated quantities.
 #'
 #' @param t_tac Numeric vector of times for each frame in minutes. We use the
 #'   time halfway through the frame as well as a zero. If a time zero frame is
@@ -23,6 +26,13 @@
 #' @param vB  Optional. The blood volume fraction.  If not specified, this will
 #'   be fitted. Recommended to perform once on a large ROI for each measurement,
 #'   and to specify this value for the remainder of the regions.
+#' @param Vnd Optional. This parameter can be used to fix the non-displaceable
+#'   volume of distribution, i.e. the K1/k2 ratio to a specific value.  If not
+#'   specified, this will be fitted.
+#' @param BPp Optional. This parameter can be used to fix the BPp value, i.e.
+#'   K1k3/k2k4 to a specific value.  If not specified, this will be fitted.
+#' @param k4 Optional. This parameter can be used to fix k4 to a specific value.
+#'   If not specified, this will be fitted.
 #' @param frameStartEnd Optional: This allows one to specify the beginning and
 #'   final frame to use for modelling, e.g. c(1,20). This is to assess time
 #'   stability.
@@ -31,16 +41,16 @@
 #' @param K1.lower Optional. Lower bound for the fitting of K1. Default is
 #'   0.0001.
 #' @param K1.upper Optional. Upper bound for the fitting of K1. Default is 1.
-#' @param k2.start Optional. Starting parameter for fitting of k2. Default is
-#'   0.1.
-#' @param k2.lower Optional. Lower bound for the fitting of k2. Default is
+#' @param Vnd.start Optional. Starting parameter for fitting of Vnd. Default is
+#'   1.
+#' @param Vnd.lower Optional. Lower bound for the fitting of Vnd. Default is
 #'   0.0001.
-#' @param k2.upper Optional. Upper bound for the fitting of k2. Default is 0.5.
-#' @param k3.start Optional. Starting parameter for fitting of k3. Default is
-#'   0.1.
-#' @param k3.lower Optional. Lower bound for the fitting of k3. Default is
+#' @param Vnd.upper Optional. Upper bound for the fitting of Vnd. Default is 10.
+#' @param BPp.start Optional. Starting parameter for fitting of BPp. Default is
+#'   1.
+#' @param BPp.lower Optional. Lower bound for the fitting of BPp. Default is
 #'   0.0001.
-#' @param k3.upper Optional. Upper bound for the fitting of k3. Default is 0.5.
+#' @param BPp.upper Optional. Upper bound for the fitting of BPp. Default is 50.
 #' @param k4.start Optional. Starting parameter for fitting of k4. Default is
 #'   0.1.
 #' @param k4.lower Optional. Lower bound for the fitting of k4. Default is
@@ -98,16 +108,17 @@
 #'
 #' @export
 
-twotcm <- function(t_tac, tac, input, weights = NULL, inpshift = NULL, vB = NULL,
-                   frameStartEnd = NULL,
-                   K1.start = 0.1, K1.lower = 0.0001, K1.upper = 1,
-                   k2.start = 0.1, k2.lower = 0.0001, k2.upper = 0.5,
-                   k3.start = 0.1, k3.lower = 0.0001, k3.upper = 0.5,
-                   k4.start = 0.1, k4.lower = 0.0001, k4.upper = 0.5,
-                   inpshift.start = 0, inpshift.lower = -0.5, inpshift.upper = 0.5,
-                   vB.start = 0.05, vB.lower = 0.01, vB.upper = 0.1,
-                   multstart_iter = 1, multstart_lower = NULL, multstart_upper = NULL,
-                   printvals = F) {
+twotcm_macro <- function(t_tac, tac, input, weights = NULL, inpshift = NULL,
+                         vB = NULL, Vnd = NULL, BPp = NULL, k4 = NULL,
+                         frameStartEnd = NULL,
+                         K1.start = 0.1, K1.lower = 0.0001, K1.upper = 1,
+                         Vnd.start = 1, Vnd.lower = 0.0001, Vnd.upper = 10,
+                         BPp.start = 1, BPp.lower = 0.0001, BPp.upper = 50,
+                         k4.start = 0.1, k4.lower = 0.0001, k4.upper = 0.5,
+                         inpshift.start = 0, inpshift.lower = -0.5, inpshift.upper = 0.5,
+                         vB.start = 0.05, vB.lower = 0.01, vB.upper = 0.1,
+                         multstart_iter = 1, multstart_lower = NULL, multstart_upper = NULL,
+                         printvals = F) {
 
   # Tidying
 
@@ -123,15 +134,15 @@ twotcm <- function(t_tac, tac, input, weights = NULL, inpshift = NULL, vB = NULL
   # Parameters
 
   start <- c(
-    K1 = K1.start, k2 = k2.start, k3 = k3.start, k4 = k4.start,
+    K1 = K1.start, Vnd = Vnd.start, BPp = BPp.start, k4 = k4.start,
     inpshift = inpshift.start, vB = vB.start
   )
   lower <- c(
-    K1 = K1.lower, k2 = k2.lower, k3 = k3.lower, k4 = k4.lower,
+    K1 = K1.lower, Vnd = Vnd.lower, BPp = BPp.lower, k4 = k4.lower,
     inpshift = inpshift.lower, vB = vB.lower
   )
   upper <- c(
-    K1 = K1.upper, k2 = k2.upper, k3 = k3.upper, k4 = k4.upper,
+    K1 = K1.upper, Vnd = Vnd.upper, BPp = BPp.upper, k4 = k4.upper,
     inpshift = inpshift.upper, vB = vB.upper
   )
 
@@ -148,6 +159,45 @@ twotcm <- function(t_tac, tac, input, weights = NULL, inpshift = NULL, vB = NULL
     vB_fitted <- F
 
     par_keepindex <- names(start) != "vB"
+    start <- start[par_keepindex]
+    lower <- lower[par_keepindex]
+    upper <- upper[par_keepindex]
+    multstart_lower <- multstart_lower[par_keepindex]
+    multstart_upper <- multstart_upper[par_keepindex]
+  }
+
+  # Sort out whether Vnd is to be fitted
+  Vnd_fitted <- T
+  if (!is.null(Vnd)) {
+    Vnd_fitted <- F
+
+    par_keepindex <- names(start) != "Vnd"
+    start <- start[par_keepindex]
+    lower <- lower[par_keepindex]
+    upper <- upper[par_keepindex]
+    multstart_lower <- multstart_lower[par_keepindex]
+    multstart_upper <- multstart_upper[par_keepindex]
+  }
+
+  # Sort out whether BPp is to be fitted
+  BPp_fitted <- T
+  if (!is.null(BPp)) {
+    BPp_fitted <- F
+
+    par_keepindex <- names(start) != "BPp"
+    start <- start[par_keepindex]
+    lower <- lower[par_keepindex]
+    upper <- upper[par_keepindex]
+    multstart_lower <- multstart_lower[par_keepindex]
+    multstart_upper <- multstart_upper[par_keepindex]
+  }
+
+  # Sort out whether k4 is to be fitted
+  k4_fitted <- T
+  if (!is.null(k4)) {
+    k4_fitted <- F
+
+    par_keepindex <- names(start) != "k4"
     start <- start[par_keepindex]
     lower <- lower[par_keepindex]
     upper <- upper[par_keepindex]
@@ -180,8 +230,11 @@ twotcm <- function(t_tac, tac, input, weights = NULL, inpshift = NULL, vB = NULL
     modeldata$input <- newvals$input
 
     formula <- paste(
-      "tac ~ twotcm_model(t_tac, input, K1, k2, k3, k4, vB",
-      ifelse( vB_fitted, yes = "", no = paste0("=", vB)),
+      "tac ~ twotcm_macro_model(t_tac, input, K1",
+      ", Vnd", ifelse( Vnd_fitted, yes = "", no = paste0("=", Vnd)),
+      ", BPp",ifelse( BPp_fitted, yes = "", no = paste0("=", BPp)),
+      ", k4",ifelse( k4_fitted, yes = "", no = paste0("=", k4)),
+      ", vB", ifelse( vB_fitted, yes = "", no = paste0("=", vB)),
       ")", sep=""
     )
 
@@ -212,8 +265,12 @@ twotcm <- function(t_tac, tac, input, weights = NULL, inpshift = NULL, vB = NULL
     inpshift_fitted <- T
 
     formula <- paste(
-      "tac ~ twotcm_fitDelay_model(t_tac, input, K1, k2, k3, k4, inpshift, vB",
-      ifelse( vB_fitted, yes = "", no = paste0("=", vB)),
+      "tac ~ twotcm_macro_fitDelay_model(t_tac, input, K1",
+      ", Vnd", ifelse( Vnd_fitted, yes = "", no = paste0("=", Vnd)),
+      ", BPp",ifelse( BPp_fitted, yes = "", no = paste0("=", BPp)),
+      ", k4",ifelse( k4_fitted, yes = "", no = paste0("=", k4)),
+      ", inpshift",
+      ", vB", ifelse( vB_fitted, yes = "", no = paste0("=", vB)),
       ")", sep=""
     )
 
@@ -274,30 +331,36 @@ twotcm <- function(t_tac, tac, input, weights = NULL, inpshift = NULL, vB = NULL
   par <- as.data.frame(as.list(coef(output)))
 
   if (inpshift_fitted == F) par$inpshift <- inpshift
-  if (vB_fitted == F)       par$vB <- vB
+  if (Vnd_fitted == F)       par$Vnd <- Vnd
+  if (BPp_fitted == F)       par$BPp <- BPp
+  if (k4_fitted == F)        par$k4 <- k4
+  if (vB_fitted == F)        par$vB <- vB
 
   par.se <- par
   par.se[1,] <- purrr::map_dbl(names(par), ~ get_se(output, .x))
   names(par.se) <- paste0(names(par.se), ".se")
 
-  par$Vt <- (par$K1 / par$k2) * (1 + par$k3 / par$k4)
-  par.se$Vt.se <- get_se(output, "(K1/k2) * (1+(k3/k4))")
+  par$Vt <- (par$Vnd + par$BPp)
+  par.se$Vt.se <- get_se(output, "Vnd + BPp")
 
-  par$Vnd <- (par$K1 / par$k2)
-  par.se$Vnd.se <- get_se(output, "(K1/k2)")
+  par$BPnd <- (par$BPp / par$Vnd)
+  par.se$BPnd.se <- get_se(output, "BPp / Vnd")
 
-  par$BPp <- (par$K1 / par$k2) * (par$k3 / par$k4)
-  par.se$BPp.se <- get_se(output, "(K1*k3)/(k2*k4)")
+  par$k3 <- (par$BPp / par$Vnd) * par$k4
+  par.se$k3.se <- get_se(output, "(BPp / Vnd) * k4")
 
-  par$BPnd <- (par$k3 / par$k4)
-  par.se$BPnd.se <- get_se(output, "(k3/k4)")
+  par$k2 <- par$K1/par$Vnd
+  par.se$k2.se <- get_se(output, "K1 / Vnd")
 
 
 
   out <- list(
     par = par, par.se = par.se,
     fit = output, tacs = tacs, input = input, weights = modeldata$weights,
-    inpshift_fitted = inpshift_fitted, vB_fitted = vB_fitted, model = "2tcm"
+    inpshift_fitted = inpshift_fitted,
+    Vnd_fitted = Vnd_fitted, BPp_fitted = BPp_fitted,
+    k4_fitted = k4_fitted, vB_fitted = vB_fitted,
+    model = "2tcm"
   )
 
   class(out) <- c("2tcm", "kinfit")
@@ -306,7 +369,7 @@ twotcm <- function(t_tac, tac, input, weights = NULL, inpshift = NULL, vB = NULL
 }
 
 
-#' Model: Two Tissue Compartment Model
+#' Model: Two Tissue Compartment Model with a Macroparameter Parameterisation
 #'
 #' This is the Two Tissue Compartment Model model itself by which predicted values are generated.
 #'
@@ -314,8 +377,8 @@ twotcm <- function(t_tac, tac, input, weights = NULL, inpshift = NULL, vB = NULL
 #' @param input Data frame containing the blood, plasma, and parent fraction concentrations over time.  This can be generated
 #' using the \code{blood_interp} function.
 #' @param K1 Parameter value for K1
-#' @param k2 Parameter value for k2
-#' @param k3 Parameter value for k3
+#' @param Vnd Parameter value for Vnd
+#' @param BPp Parameter value for BPp
 #' @param k4 Parameter value for k4
 #' @param vB Parameter value for vB
 #'
@@ -323,19 +386,22 @@ twotcm <- function(t_tac, tac, input, weights = NULL, inpshift = NULL, vB = NULL
 #'
 #' @examples
 #' \dontrun{
-#' twotcm_model(t_tac, input, K1 = 0.1, k2 = 0.08, k3 = 0.05, k4 = 0.02, vB = 0.05)
+#' twotcm_macro_model(t_tac, input, K1 = 0.1, Vnd = 1, BPp = 2, k4 = 0.02, vB = 0.05)
 #' }
 #'
 #' @author Granville J Matheson, \email{mathesong@@gmail.com}
 #'
 #' @export
 
-twotcm_model <- function(t_tac, input, K1, k2, k3, k4, vB) {
+twotcm_macro_model <- function(t_tac, input, K1, Vnd, BPp, k4, vB) {
   interptime <- input$Time
   step <- interptime[2] - interptime[1]
 
   i_blood <- input$Blood
   aif <- input$AIF
+
+  k2 <- K1 / Vnd
+  k3 <- (BPp / Vnd) * k4
 
   delta <- sqrt((k2 + k3 + k4)^2 - 4 * k2 * k4)
   th1 <- (k2 + k3 + k4 + delta) / 2
@@ -357,7 +423,7 @@ twotcm_model <- function(t_tac, input, K1, k2, k3, k4, vB) {
 }
 
 
-#' Model: Two Tissue Compartment Model with Delay
+#' Model: Two Tissue Compartment Model with Delay with a Macroparameter Parameterisation
 #'
 #' This is the Two Tissue Compartment Model model itself by which predicted values are generated, which includes fitting of the
 #' delay, inpshift.
@@ -366,8 +432,8 @@ twotcm_model <- function(t_tac, input, K1, k2, k3, k4, vB) {
 #' @param input Data frame containing the blood, plasma, and parent fraction concentrations over time.  This can be generated
 #' using the \code{blood_interp} function.
 #' @param K1 Parameter value for K1
-#' @param k2 Parameter value for k2
-#' @param k3 Parameter value for k3
+#' @param Vnd Parameter value for Vnd
+#' @param BPp Parameter value for BPp
 #' @param k4 Parameter value for k4
 #' @param inpshift Parameter value for inpshift, the delay.
 #' @param vB Parameter value for vB
@@ -376,14 +442,15 @@ twotcm_model <- function(t_tac, input, K1, k2, k3, k4, vB) {
 #'
 #' @examples
 #' \dontrun{
-#' twotcm_model(t_tac, input, K1 = 0.1, k2 = 0.08, k3 = 0.05, k4 = 0.02, vB = 0.05)
+#' twotcm_macro_fitDelay_model(t_tac, input, K1 = 0.1, Vnd = 1, BPp = 2,
+#'                             k4 = 0.02, inpshift = 0, vB = 0.05)
 #' }
 #'
 #' @author Granville J Matheson, \email{mathesong@@gmail.com}
 #'
 #' @export
 
-twotcm_fitDelay_model <- function(t_tac, input, K1, k2, k3, k4, inpshift, vB) {
+twotcm_macro_fitDelay_model <- function(t_tac, input, K1, Vnd, BPp, k4, inpshift, vB) {
   newvals <- shift_timings(t_tac, rep(1, length(t_tac)), input, inpshift) # Using ones instead of tac as don't need it
 
   t_tac <- newvals$t_tac
@@ -393,6 +460,9 @@ twotcm_fitDelay_model <- function(t_tac, input, K1, k2, k3, k4, inpshift, vB) {
 
   interptime <- newvals$input$Time
   step <- interptime[2] - interptime[1]
+
+  k2 <- K1 / Vnd
+  k3 <- (BPp / Vnd) * k4
 
   delta <- sqrt((k2 + k3 + k4)^2 - 4 * k2 * k4)
   th1 <- (k2 + k3 + k4 + delta) / 2
@@ -411,111 +481,4 @@ twotcm_fitDelay_model <- function(t_tac, input, K1, k2, k3, k4, inpshift, vB) {
   outtac <- pracma::interp1(interptime, i_outtac, t_tac)
 
   return(outtac)
-}
-
-
-#' Plot: Two Tissue Compartment Model
-#'
-#' Function to visualise the fit of the Two Tissue Compartment Model to data.
-#'
-#' @param twotcmout The output object of the 2TCM fitting procedure.
-#' @param roiname Optional. The name of the Target Region to see it on the plot.
-#'
-#' @return A ggplot2 object of the plot.
-#'
-#' @examples
-#'
-#' data(pbr28)
-#'
-#' t_tac <- pbr28$tacs[[2]]$Times / 60
-#' tac <- pbr28$tacs[[2]]$FC
-#' weights <- pbr28$tacs[[2]]$Weights
-#'
-#' input <- blood_interp(
-#'   pbr28$procblood[[2]]$Time / 60, pbr28$procblood[[2]]$Cbl_dispcorr,
-#'   pbr28$procblood[[2]]$Time / 60, pbr28$procblood[[2]]$Cpl_metabcorr,
-#'   t_parentfrac = 1, parentfrac = 1
-#' )
-#'
-#' fit <- twotcm(t_tac, tac, input, weights, inpshift = 0.1, vB = 0.05)
-#'
-#' plot_2tcmfit(fit)
-#' @author Granville J Matheson, \email{mathesong@@gmail.com}
-#'
-#' @import ggplot2
-#'
-#' @export
-
-plot_2tcmfit <- function(twotcmout, roiname = NULL) {
-  if (is.null(roiname)) {
-    roiname <- "ROI"
-  }
-
-  measureddf <- data.frame(
-    Time = twotcmout$tacs$Time,
-    Radioactivity = twotcmout$tacs$Target,
-    Weights = weights(twotcmout$fit),
-    Region = paste0(roiname, ".Measured")
-  )
-
-  inputdf <- data.frame(
-    Time = twotcmout$input$Time,
-    Radioactivity = twotcmout$input$AIF,
-    Weights = 1,
-    Region = "AIF"
-  )
-
-  i_fit <- predict(twotcmout$fit, newdata = list(
-    t_tac = twotcmout$input$Time,
-    tac = pracma::interp1(
-      twotcmout$tacs$Time,
-      twotcmout$tacs$Target,
-      twotcmout$input$Time,
-      method = "linear"
-    )
-  ))
-
-  # Correct for TAC shifting
-  if( twotcmout$inpshift_fitted ) {
-    mod_tac_start <- head(twotcmout$fit$m$getEnv()$t_tac)[-1]
-    dat_tac_start <- head(twotcmout$tacs$Time)[-1]
-    diffs <- dat_tac_start - mod_tac_start
-
-    equal_diffs <- abs(max(diffs) - min(diffs)) < 10e-3
-
-    if(equal_diffs) {
-      tacshift <- mean(diffs)
-    } else {
-      tacshift <- 0
-      warning("Cannot infer whether TAC was shifted. Fit line might be misaligned in time.")
-    }
-  } else {
-    tacshift <- 0
-  }
-
-  fitdf <- data.frame(
-    Time = twotcmout$input$Time + tacshift,
-    Radioactivity = i_fit,
-    Weights = 1, Region = paste0(roiname, ".Fitted")
-  )
-
-  plotdf <- rbind(inputdf, measureddf, fitdf)
-
-  plotdf$Region <- forcats::fct_inorder(factor(plotdf$Region))
-
-  myColors <- RColorBrewer::brewer.pal(3, "Set1")
-  names(myColors) <- levels(plotdf$Region)
-  colScale <- scale_colour_manual(name = "Region", values = myColors)
-
-  #   if(roiname != 'ROI') {
-  #     plotdf$Region[plotdf$Region=='Measured'] = paste0(roiname, '')
-  #   }
-
-  outplot <- ggplot(plotdf, aes(x = Time, y = Radioactivity, colour = Region)) + colScale +
-    geom_point(data = subset(plotdf, plotdf$Region == paste0(roiname, ".Measured")), aes(shape = "a", size = Weights)) +
-    geom_line(data = subset(plotdf, plotdf$Region != paste0(roiname, ".Measured"))) +
-    guides(shape = "none", color = guide_legend(order = 1)) + scale_size(range = c(1, 3)) + coord_cartesian(ylim = c(0, max(measureddf$Radioactivity) * 1.5))
-
-  # print(outplot)
-  return(outplot)
 }
