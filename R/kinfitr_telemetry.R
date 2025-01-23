bf530b9a4d035208284285b22a8cecf053ca1037
Anthony Galassi <2317139-anthonygalassi@users.noreply.gitlab.com>
2025-01-22 23:01:40 -0500

    sends a request on library load

--- R/kinfitr_telemetry.R
+++ R/kinfitr_telemetry.R
@@ -1,7 +1,8 @@
-library(httr2)
+#' @importFrom httr2 request req_headers req_retry req_timeout req_perform
+#' @importFrom httr2 resp_body_json req_body_json
 
-get_url <- "http://127.0.0.1:8000/check/kinfitr/"
-post_url <- "http://127.0.0.1:8000/kinfitr/"
+get_url <- "http://54.144.240.214/check/kinfitr/"
+post_url <- "http://54.144.240.214/kinfitr/"
 
 get_telemetry <- function(url = get_url, number_of_records = 0) {
   # checks to see what's been posted to the url endpoint, should return location,
@@ -17,32 +18,35 @@ get_telemetry <- function(url = get_url, number_of_records = 0) {
 #' Send telemetry data
 #' @keywords internal
 send_telemetry <- function(telemetry_json_data, url = post_url) {
-  # Accepts a list of telemetry data (worth transmitting of course)
-  # and converts it such that it can be sent as a json to the endpoint.
-  # Additionally, checks to to see if environment variable
-  # KINFITR_NO_TRACK is set if it is, this does nothing.
+  # Add debug messages
+  message("Debug: Starting send_telemetry")
+  message("Debug: Telemetry data: ", toString(telemetry_json_data))
 
   no_track <- Sys.getenv("KINFITR_NO_TRACK")
+  message("Debug: KINFITR_NO_TRACK value: ", no_track)
 
   if (tolower(no_track) == "true") {
+    message("Debug: Tracking disabled")
     return(NULL)
   } else {
+    message("Debug: Attempting to send telemetry")
     try(
       {
         req <- request(url)
-        req |> req_retry(max_tries = 3)
-        req |> req_timeout(3)
+        # Fix the request chain - these need to be assigned
+        req <- req_retry(req, max_tries = 3)
+        req <- req_timeout(req, 3)
+
+        message("Debug: Sending to URL: ", url)
         response <- req |>
           req_body_json(data = telemetry_json_data) |>
           req_perform()
+
+        message("Debug: Response status: ", response$status_code)
         return_values <- list(status_code = response$status_code)
         return(return_values)
       },
-      silent = TRUE
+      silent = FALSE # Changed to FALSE to see any errors
     )
   }
 }
-
-
-# list_to_send <- list("part1" = "one", "part2" = "new")
-# end_telemetry(list_to_send)
--- R/zzz.R
+++ R/zzz.R
@@ -1,24 +1,10 @@
-library(httr2)
-
-get_url <- "http://127.0.0.1:8000/check/kinfitr/"
-post_url <- "http://127.0.0.1:8000/kinfitr/"
-
-get_telemetry <- function(url = get_url, number_of_records = 0) {
-  # checks to see what's been posted to the url endpoint, should return
-  # location and any other data that gets put there with send_telemetry
-  req <- request(paste(url, as.character(number_of_records), sep = ""))
-  req |> req_headers("Accept" = "application/json")
-  req |> req_retry(max_tries = 3)
-  req |> req_timeout(3)
-  response <- req_perform(req)
-  return(resp_body_json(response))
-}
-
-.onLoad <- function(libname, pkgname) {
+.onAttach <- function(libname, pkgname) {
+  message("Loading kinfitr")
+  message("new debug message")
   # Display opt-out message
   if (
-      isFALSE(getOption("kinfitr.no_track")) ||
-        isFALSE(Sys.getenv("KINFITR_NO_TRACK"))) {
+      !isTRUE(getOption("kinfitr.no_track")) &&
+        Sys.getenv("KINFITR_NO_TRACK") != "TRUE") {
     message(
       "Opt-out of sending tracking information to the KinFitR developers. ",
       "This information provides an indicator of real world usage crucial for ",
@@ -29,11 +15,15 @@ get_telemetry <- function(url = get_url, number_of_records = 0) {
       "KINFITR_NO_TRACK to FALSE"
     )
   }
-  # Check if user has opted out
+  # # Check if user has opted out
   if (
       isTRUE(getOption("kinfitr.no_track")) ||
         isTRUE(Sys.getenv("KINFITR_NO_TRACK"))) {
+    # do nothing
+    NULL
+  } else {
     # Send telemetry when package is loaded
+    message("Sending telemetry")
     send_telemetry(list("kinfitr_usage" = "package_loaded"))
   }
 }
