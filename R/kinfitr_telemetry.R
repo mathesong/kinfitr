c441aaa326548e5c8880281bde937e365228693f
Anthony Galassi <28850131+bendhouseart@users.noreply.github.com>
2025-02-05 11:39:07 -0500

    Apply suggestions from code review

--- R/kinfitr_telemetry.R
+++ R/kinfitr_telemetry.R
@@ -30,15 +30,15 @@ send_telemetry <- function(telemetry_json_data, url = post_url) {
     message("Debug: Attempting to send telemetry")
     try(
       {
-        req <- request(url)
+        req <- httr2::request(url)
         # Fix the request chain - these need to be assigned
         req <- httr2::req_retry(req, max_tries = 3)
         req <- httr2::req_timeout(req, 3)
 
         message("Debug: Sending to URL: ", url)
         response <- req |>
-          req_body_json(data = telemetry_json_data) |>
-          req_perform()
+          httr2::req_body_json(data = telemetry_json_data) |>
+          httr2::req_perform()
 
         message("Debug: Response status: ", response$status_code)
         return_values <- list(status_code = response$status_code)
